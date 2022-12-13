2e3a01c026475d0295f8beb366828798e6c19971
murasaqi <uma9266@gmail.com>
2022-12-14 00:42:08 +0900

    マテリアルのカラー同期にIntensityが0になるほど黒にさせるかどうかのFlagを追加

--- jp.iridescenet.stagelightmaneuver/Runtime/Data/SyncLightMaterialProperty.cs
+++ jp.iridescenet.stagelightmaneuver/Runtime/Data/SyncLightMaterialProperty.cs
@@ -3,12 +3,14 @@
     public class SyncLightMaterialProperty:SlmAdditionalProperty
     {
         public SlmToggleValue<float> intensitymultiplier;
-
+        public SlmToggleValue<bool> brightnessDecreasesToBlack;
         public SyncLightMaterialProperty()
         {
             propertyName = "Sync Light Material";
             propertyOverride = false;
             intensitymultiplier = new SlmToggleValue<float>() { value = 1f };
+            brightnessDecreasesToBlack = new SlmToggleValue<bool>() { value = false };
+            
         }
         
         public SyncLightMaterialProperty(SyncLightMaterialProperty other)
@@ -16,6 +18,7 @@
             propertyName = other.propertyName;
             propertyOverride = other.propertyOverride;
             intensitymultiplier = new SlmToggleValue<float>(){value = other.intensitymultiplier.value};
+            brightnessDecreasesToBlack = new SlmToggleValue<bool>() { value = other.brightnessDecreasesToBlack.value };
         }
     }
 }
