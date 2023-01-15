5493c39cd7ac157a002f3859da6e90de78c21e79
murasaqi <uma9266@gmail.com>
2023-01-16 01:54:41 +0900

    Add namespace

--- jp.iridescenet.stagelightmaneuver/Runtime/Untility/AutoSpotTwinner.cs
+++ jp.iridescenet.stagelightmaneuver/Runtime/Untility/AutoSpotTwinner.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
-public class AutoSpotTwinner : MonoBehaviour
+namespace StageLightManeuver
 {
-
-    public Transform target;
-    [Range(0.001f,0.3f)]public float speed = 0.1f;
-    // Start is called before the first frame update
-    void Start()
+    [ExecuteAlways]
+    public class AutoSpotTwinner : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(target == null) return;
-        
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), speed);
+        public Transform target;
+
+        [Range(0.001f, 0.3f)] public float speed = 0.1f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (target == null) return;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(target.position - transform.position), speed);
+        }
     }
 }
